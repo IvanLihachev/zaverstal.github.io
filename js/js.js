4844bc8d4c54c6d2cc69a750d44972954322dd05
Ivan Lihachev <design@dinix.ru>
2018-01-22 16:51:48 +0500

    no message

--- js/js.js
+++ js/js.js
@@ -1,16 +1,50 @@
 $(document).ready(function() {
 
-	var $grid = $('.grid').packery();
-	if($(window).width() > 640) {
-        $grid.imagesLoaded( function() {
-            $grid.packery({
-                itemSelector: '.item',
-                percentPosition: true
-            });
-        });
-	}   else{
-        $grid.packery('destroy');
-    }
+
+    // Масонри
+
+    var $container = $('.grid');
+    $container.imagesLoaded(function(){
+      $container.masonry({
+            itemSelector : '.grid-item',
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+      });
+    });
+
+    $('.grid').masonry({
+        // options
+        itemSelector : '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    }).masonry('reload');
+
+
+    // Фильтрация
+
+    // $('.grid').isotope({
+    //   // options
+    //   itemSelector: '.grid-item',
+    //   layoutMode: 'fitRows'
+    // });
     
+    // var $grid = $('.grid').isotope({
+    //   // options
+    // });
+    // // filter items on button click
+    // $('.filter__group').on( 'click', 'button', function() {
+    //     var filterValue = $(this).attr('data-filter');
+    //     $grid.isotope({ filter: filterValue }).masonry('reload');
+    // });
+
+    
+    // // Кнопки фильтрации
+
+    // $('button').click(function() {
+    //     $('button').removeClass('filter__button_checked'),
+    //     $(this).addClass('filter__button_checked')
+    // });
+
+
 });
 
